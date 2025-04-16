95b7eba3fc4addb9092c3f93eadf1f32c3e69aff
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-04-16 14:39:17 +0200

    Starting to add some example code to the repository.

---
+++ CUDAExamples/src/01_LinearTransformAlg/LinearTransformCUDA.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2002-2025 CERN for the benefit of the ATLAS collaboration
+#ifndef CUDAEXAMPLES_LINEARTRANSFORMCUDA_H
+#define CUDAEXAMPLES_LINEARTRANSFORMCUDA_H
+
+// Framework include(s).
+#include "AthenaBaseComps/AthReentrantAlgorithm.h"
+
+namespace GPUTutorial {
+
+
+} // namespace GPUTutorial
+
+#endif // CUDAEXAMPLES_LINEARTRANSFORMCUDA_H
--- build_env.sh
+++ build_env.sh
@@ -6,3 +6,16 @@ source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
 
 # Set up Athena-25.0.29.
 asetup Athena,25.0.29
+
+# Set up CUDA from CVMFS.
+CUDA_DIR="/cvmfs/projects.cern.ch/lcg/releases/cuda/12.8/`uname -m`-linux"
+if [ -d "${CUDA_DIR}" ]; then
+   export PATH="${CUDA_DIR}/bin"${PATH:+:${PATH}}
+   export LD_LIBRARY_PATH="${CUDA_DIR}/lib64:${CUDA_DIR}/extras/CUPTI/lib64$"{LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+   export CUDACXX=`which nvcc`
+   echo "Configured CUDA from: ${CUDA_DIR}"
+else
+   echo "WARNING:"
+   echo "WARNING: CUDA not available from CVMFS. Please set it up by hand!"
+   echo "WARNING:"
+fi
