809e274832358f61a09d74bbc254ea9940613642
Beojan Stanislaus <beojan@gmail.com>
2025-04-28 01:52:14 -0700

    Async exercise (#10)
    * Add start of asynchronous exercise
    
    * More of the exercise
    
    * Completed exercise
    
    * Unsolve async exercises
    
    * Add asynchronous algorithms notebook
    
    * Get rid of warnings from the build of the starting code.
    
    * Small tweaks to the asynchronous algorithm notebook.
    
    ---------
    
    Co-authored-by: Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>

---
+++ CUDAExamples/src/03_Asynchronous/JetPullCUDAAlg.cxx
@@ -0,0 +1,151 @@
+// Copyright (C) 2002-2025 CERN for the benefit of the ATLAS collaboration
+
+// Local include(s).
+#include "JetPullCUDAAlg.h"
+
+// Framework include(s).
+#include "StoreGate/ReadHandle.h"
+#include "StoreGate/WriteHandle.h"
+#include "xAODCore/AuxContainerBase.h"
+#include "xAODCore/ShallowCopy.h"
+#include "xAODJet/JetContainer.h"
+#include "xAODJet/Jet.h"
+
+// VecMem include(s).
+#include <vecmem/memory/cuda/host_memory_resource.hpp>
+// #include <vecmem/memory/cuda/device_memory_resource.hpp>
+#include <vecmem/memory/pool_memory_resource.hpp>
+#include <vecmem/memory/synchronized_memory_resource.hpp>
+#include <vecmem/utils/cuda/copy.hpp>
+
+// STL includes(s)
+#include <format>
+#include <span>
+#include <vector>
+
+namespace GPUTutorial
+{
+   struct JetPullCUDAAlg::MemoryResources
+   {
+      /// Uncached host memory resource
+      vecmem::cuda::host_memory_resource m_plainHostMR;
+      /// Cached host memory resource
+      vecmem::pool_memory_resource m_cachedHostMR{m_plainHostMR};
+      /// Synchronized and cached host memory resource
+      vecmem::synchronized_memory_resource m_hostMR{m_cachedHostMR};
+
+      std::pmr::memory_resource* hostMR();
+   };
+
+   std::pmr::memory_resource* JetPullCUDAAlg::MemoryResources::hostMR() {
+      return &m_hostMR;
+   }
+
+   JetPullCUDAAlg::JetPullCUDAAlg(const std::string &name,
+                                              ISvcLocator *svcloc)
+       : AthAsynchronousAlgorithm(name, svcloc) {}
+
+   JetPullCUDAAlg::~JetPullCUDAAlg() = default;
+
+   StatusCode JetPullCUDAAlg::initialize()
+   {
+      // Set up the memory resources.
+      m_memoryResources = std::make_unique<MemoryResources>();
+
+      // Set up the input and output keys.
+      ATH_CHECK(m_inputKey.initialize());
+      ATH_CHECK(m_outputKey.initialize());
+
+      // Return gracefully.
+      return StatusCode::SUCCESS;
+   }
+
+   StatusCode JetPullCUDAAlg::execute(const EventContext &ctx) const
+   {
+      // Get the input container.
+      SG::ReadHandle inputJets(m_inputKey, ctx);
+
+      // Collect required data on the host:
+      //    - Number of jets
+      //    - Number of constituents of each jet
+      //    - pt, eta, phi for jets
+      //    - pt, eta, phi for constituents
+      std::size_t nJets = inputJets->size();
+      if (nJets == 0) {
+         return StatusCode::SUCCESS;
+      }
+      std::pmr::vector<std::size_t> nConstituents(m_memoryResources->hostMR());
+      // // Is this worth it?
+      // std::pmr::vector<float> jetPt(nJets, m_memoryResources->hostMR());
+      // std::pmr::vector<float> jetEta(nJets, m_memoryResources->hostMR());
+      // std::pmr::vector<float> jetPhi(nJets, m_memoryResources->hostMR());
+      std::pmr::vector<float> constPt(m_memoryResources->hostMR());
+      std::pmr::vector<float> constEta(m_memoryResources->hostMR());
+      std::pmr::vector<float> constPhi(m_memoryResources->hostMR());
+
+      // Get jet data
+      static const SG::AuxElement::ConstAccessor<float> ptAcc("pt");
+      static const SG::AuxElement::ConstAccessor<float> etaAcc("eta");
+      static const SG::AuxElement::ConstAccessor<float> phiAcc("phi");
+      std::span<const float> jetPt(ptAcc.getDataArray(*inputJets), nJets);
+      std::span<const float> jetEta(etaAcc.getDataArray(*inputJets), nJets);
+      std::span<const float> jetPhi(phiAcc.getDataArray(*inputJets), nJets);
+      // std::memcpy(jetPt.data(), ptAcc.getDataArray(*inputJets), nJets * sizeof(float));
+      // std::memcpy(jetEta.data(), etaAcc.getDataArray(*inputJets), nJets * sizeof(float));
+      // std::memcpy(jetPhi.data(), phiAcc.getDataArray(*inputJets), nJets * sizeof(float));
+
+      // Get constituent data. We loop twice so we can allocate the constituent arrays in one go.
+      std::size_t totalConstituents = 0;
+      nConstituents.reserve(nJets);
+      for (const auto& jet : *inputJets) {
+         std::size_t nConst = jet->numConstituents();
+         totalConstituents += nConst;
+         nConstituents.push_back(nConst);
+      }
+      
+      constPt.reserve(totalConstituents);
+      constEta.reserve(totalConstituents);
+      constPhi.reserve(totalConstituents);
+      for (const auto& jet : *inputJets) {
+         for (const auto* c : jet->getConstituents()) {
+            constPt.push_back(c->pt());
+            constEta.push_back(c->eta());
+            constPhi.push_back(c->phi());
+         }
+      }
+   
+      // Create host buffers to store results
+      std::pmr::vector<float> jetPullEta(nJets, m_memoryResources->hostMR());
+      std::pmr::vector<float> jetPullPhi(nJets, m_memoryResources->hostMR());
+
+      // Run GPU code
+      ATH_CHECK(deviceExecute(jetPt, jetEta, jetPhi, nConstituents, constPt, constEta, constPhi,
+                               jetPullEta, jetPullPhi));
+      
+      // Save output
+      // Get an std::pair of unique_ptrs back
+      auto outputJetsSC = xAOD::shallowCopyContainer(*inputJets, ctx);
+      auto&& [outputJets, outputAux] = outputJetsSC;
+      {
+         SG::Accessor<float> pullEtaAcc("pullEta");
+         SG::Accessor<float> pullPhiAcc("pullPhi");
+         std::size_t i = 0;
+         for(auto jet : *outputJets) {
+            pullEtaAcc(*jet) = jetPullEta[i];
+            pullPhiAcc(*jet) = jetPullPhi[i];
+            ++i;
+         }
+      }
+      if (nJets > 0) {
+         ATH_MSG_INFO(std::format("Jet 0 with {} components has pull vector [{}, {}]", nConstituents[0], jetPullEta[0], jetPullPhi[0]));
+      }
+
+      // Record
+      SG::WriteHandle output(m_outputKey, ctx);
+      ATH_CHECK(output.record(std::move(outputJets), std::move(outputAux)));
+
+      // Return gracefully.
+      return StatusCode::SUCCESS;
+   }
+
+} // namespace GPUTutorial
--- CUDAExamples/src/components/CUDAExamples_entries.cxx
+++ CUDAExamples/src/components/CUDAExamples_entries.cxx
@@ -3,7 +3,10 @@
 // Local include(s).
 #include "../01_LinearTransform/LinearTransformCUDAAlg.h"
 #include "../02_xAODCalib/ElectronCalibCUDAAlg.h"
+#include "../03_Asynchronous/JetPullCUDAAlg.h"
 
 // Declare the component(s).
 DECLARE_COMPONENT(GPUTutorial::LinearTransformCUDAAlg)
 DECLARE_COMPONENT(GPUTutorial::ElectronCalibCUDAAlg)
+DECLARE_COMPONENT(GPUTutorial::JetPullCUDAAlg)
+
